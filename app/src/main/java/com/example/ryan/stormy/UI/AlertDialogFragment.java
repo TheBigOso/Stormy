24525dd6e2e6d25857606bc826603f59a4922f2b
thebigoso <ryan.ruddell@gmail.com>
2015-10-09 09:37:09 -0700

    120

---
+++ app/src/main/java/com/example/ryan/stormy/UI/AlertDialogFragment.java
@@ -0,0 +1,27 @@
+package com.example.ryan.stormy.UI;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.example.ryan.stormy.R;
+
+/**
+ * Created by ryanr on 10/2/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_ok_button, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
---
+++ app/src/main/java/com/example/ryan/stormy/UI/DailyForecastActivity.java
@@ -0,0 +1,50 @@
+package com.example.ryan.stormy.UI;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.ryan.stormy.R;
+import com.example.ryan.stormy.adapters.DayAdapter;
+import com.example.ryan.stormy.weather.Day;
+
+import java.util.Arrays;
+
+public class DailyForecastActivity extends ListActivity {
+
+ private Day[] mDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        setListAdapter(adapter);
+
+//        String[] daysOfTheWeek = {
+//                "Sunday",
+//                "Monday",
+//                "Tuesday",
+//                "Wednesday",
+//                "Thursday",
+//                "Friday",
+//                "Saturday"
+//        };
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                daysOfTheWeek);
+//
+//        setListAdapter(adapter);
+
+    }
+
+
+}
